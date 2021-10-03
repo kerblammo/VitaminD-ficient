89aa0a03b36199aabe856857534ca952254f259f
Peter Adam <padamCKB@hotmail.com>
2021-10-03 13:02:57 -0300

    The pause screen now displays properly, and has the option to mute both sound effects and background music

--- LDJamOctober2021/Assets/Scripts/UI.cs
+++ LDJamOctober2021/Assets/Scripts/UI.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class UI : MonoBehaviour
 {
+    [Header("Game Over Screen")]
     [SerializeField] GameObject GameOverScreen;
     [SerializeField] float gameOverScreenDelay;
     [SerializeField] Text ScoreLabel;
+
+    [Header("Pause Menu")]
+    [SerializeField] GameObject PauseScreen;
+    [SerializeField] Toggle BGMToggle;
+    [SerializeField] AudioMixer BGMMixer;
+    [SerializeField] Toggle SFXToggle;
+    [SerializeField] AudioMixer SFXMixer;
+
     bool isPaused = false;
 
     public void ShowGameOverScreen()
@@ -33,15 +43,29 @@ public class UI : MonoBehaviour
         Application.Quit();
     }
 
+    public void ToggleBGM()
+    {
+        float volume = BGMToggle.isOn ? 0f : -80f;
+        BGMMixer.SetFloat("Volume", volume);
+    }
+
+    public void ToggleSFX()
+    {
+        float volume = SFXToggle.isOn ? 0f : -80f;
+        SFXMixer.SetFloat("Volume", volume);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (isPaused)
             {
+                PauseScreen.SetActive(false);
                 Time.timeScale = 1f;
             } else
             {
+                PauseScreen.SetActive(true);
                 Time.timeScale = 0f;
             }
             isPaused = !isPaused;
